4cf90a67fb01d805d6e896290d3052d5896dc7ca
Alexandre Lachèze <alexandre.lacheze@gmail.com>
2019-08-30 19:09:12 +0200

    Properly implements `inspect`

--- src/jstutils/inspect.ts
+++ src/jstutils/inspect.ts
@@ -1,4 +1,6 @@
-export default function inspect(smthing: any) {
-  // todo find something better
-  return JSON.stringify(smthing);
+// @ts-ignore nodeJS typescript not loaded
+import * as util from 'util';
+
+export default function inspect(smthing: unknown) {
+  return util.inspect(smthing);
 }
