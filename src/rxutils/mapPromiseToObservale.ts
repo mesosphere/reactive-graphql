5ff50941621dd8548d46805f076c79a72e2970b7
Alexandre Lachèze <alexandre.lacheze@gmail.com>
2019-09-19 20:12:50 +0200

    Fix typo

--- src/rxutils/mapPromiseToObservale.ts
+++ src/rxutils/mapPromiseToObservale.ts
@@ -5,11 +5,11 @@ import { switchMap } from "rxjs/operators";
  * Build an Observable given a Promise's resolved value.
  *
  * @param promise 
- * @param obserbaleFn 
+ * @param observableFn 
  */
 export default function mapPromiseToObservale<TPromiseValue, TObservableValue>(
   promise: Promise<TPromiseValue>,
-  obserbaleFn: (resolved: TPromiseValue) => Observable<TObservableValue>
+  observableFn: (resolved: TPromiseValue) => Observable<TObservableValue>
 ): Observable<TObservableValue> {
-  return from(promise).pipe(switchMap(obserbaleFn));
+  return from(promise).pipe(switchMap(observableFn));
 };
